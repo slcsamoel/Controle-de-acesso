822ef1e14a4f56c2ce05c0206e1e8f3acf4a7b96
slcsamoel <samuellopescosta@gmail.com>
2019-08-30 12:45:44 -0300

    inserindo Função na validaca_login.php

--- controller/validacao_login.php
+++ controller/validacao_login.php
@@ -1,14 +1,21 @@
 <?php
-
-
-
-
-
-      
-    
-
-
-
+require_once ( ' ../model/Usuario.php ' );
+require_once ( ' ../model/Pessoa.php ' );
+require_once ( ' ../controller/conexao_banco.php ' );
+$ class_usuario  =  novo  Usuario ();
+$ class_usuario -> setNome ( $ nome  =  $ _POST [ ' usuario ' ]);
+$ class_usuario -> setNome ( $ senha  =  $ _POST [ ' senha ' ]);
+    função  logar ( $ link , $ nome , $ senha ) {
+        $ resultado  =  mysqli_query ( $ link , " SELECT usuario e senha FROM tb_usuario WHERE usuario =  ' " . $ nome . " ' AND senha = ' " . $ senha . " ' " );
+        
+        if (mysqli_num_rows ( $ resultado ) >  0 ) { / / lOGIN COM SUCESSO
+            cabeçalho ( ' localização: ../principal.php ' );
+            eco ( ' Logado ' );
+        } mais {
+            echo ( ' não ' );
+        }   
+}
+logar ( $ link , $ nome , $ senha );
 
 
 ?>
